7de8f4ece820d7a9c0f240edd21c0e2bf025881f
ritik337 <59756227+ritik337@users.noreply.github.com>
2024-02-11 01:06:06 +0530

    trying to improve designing and logical sides of this project

---
+++ placeholderChanger.js
@@ -0,0 +1,7 @@
+function placeholderChange(){
+    let inputbox = document.getElementsByName('city'); 
+    let suggestion_arr = ["Hodal","Palwal","Kosi Kalan","Vrindavan","Gurugram"]
+    let length_arr = suggestion_arr.length
+    let count = 0; 
+}
+placeholderChange()
